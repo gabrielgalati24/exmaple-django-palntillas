f0c5b324b917574a4717340113ffaae488b7463f
GabrielGalatiUpa <gabriel.upasoft@gmail.com>
2021-09-19 00:02:26 -0400

    Empty django app

--- furry/settings.py
+++ furry/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://plantilla-personajes-furrys.herokuapp.com/' ,'127.0.0.1']
 
 
 # Application definition
